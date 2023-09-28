fa099ddf76f32cd628c5ae25adb5858f8d18cf3e
JoseP <jose.p2005@aluno.isc.edu.br>
2023-09-28 08:34:11 -0300

    Modelo MVC

---
+++ src/Controle/ex1.java
@@ -0,0 +1,5 @@
+package Controle;
+
+public class ex1 {
+
+}
---
+++ src/Modelo/ex2.java
@@ -0,0 +1,10 @@
+package Modelo;
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
